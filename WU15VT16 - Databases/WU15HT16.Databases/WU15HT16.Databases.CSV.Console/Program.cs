65a4f7b1eabd4bba1da4671e35bf8f9b04f5e2b6
Lord Prashant <lord_prashant@hotmail.com>
2018-05-20 15:23:27 +0200

    First push

---
+++ WU15VT16 - Databases/WU15HT16.Databases/WU15HT16.Databases.CSV.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace WU15HT16.Databases.CSV.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = Directory.GetCurrentDirectory() + "../../../Data/Products.csv";
+        }
+    }
+}
---
+++ WU15VT16 - Databases/WU15HT16.Databases/WU15HT16.Databases.Files.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace WU15HT16.Databases.FilesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseDirectoryPath = Directory.GetCurrentDirectory() + @"..\..\..\";
+            var dataDirectoryPath = Path.Combine(baseDirectoryPath, "Data");
+            var filePath = Path.Combine(dataDirectoryPath, "Information.txt");
+
+            Console.ReadLine();
+
+            Directory.CreateDirectory(dataDirectoryPath);
+
+            StreamWriter streamWriter = File.CreateText(filePath);
+            streamWriter.Close();
+
+            //streamWriter = new StreamWriter(filePath);
+            //streamWriter.WriteLine("{0}: {1}", DateTime.Now, "Cancer!");
+            //streamWriter.Close();
+
+            //streamWriter = File.AppendText(filePath);
+            //streamWriter.WriteLine("{0}: {1}", DateTime.Now, "MORE AIDS!");
+            //streamWriter.Close();
+
+            MemoryStream memoryStream = new MemoryStream();
+            streamWriter = new StreamWriter(memoryStream);
+
+            Console.ReadLine();
+        }
+    }
+}
